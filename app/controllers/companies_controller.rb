9a29ee6c2be533342d4791c8bc458a2d2d750005
Alison Souza <alisonmsouza@yahoo.com.br>
2016-06-26 19:11:43 +0000

    adjustments about devise

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,4 @@
-class CompaniesController < ApplicationController
+﻿class CompaniesController < ApplicationController
   before_action :set_company, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_member!
 
